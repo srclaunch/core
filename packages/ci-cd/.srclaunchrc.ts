7531a2a93dd9cd4e7341a9aacf4bb42604dbf4e2
Steven Bennett <steven@budgetbloom.com>
2022-06-12 17:49:07 -0700

    Update

--- packages/ci-cd/.srclaunchrc.ts
+++ packages/ci-cd/.srclaunchrc.ts
@@ -18,21 +18,6 @@ import {
   GitHubWorkflow,
   PackageManager,
 } from '@srclaunch/types';
-import {
-  BuildProject,
-  BuildProjectDocs,
-  Checkout,
-  GetPackageMetadata,
-  UploadArtifact,
-  GetBranch,
-  SetupNode,
-  SetupPNPM,
-  RunTests,
-  UploadCoverageToCoveralls,
-  DownloadArtifact,
-  PNPMPublish,
-  WriteNPMConfig,
-} from '@srclaunch/actions';
 
 export default <LibraryConfig>{
   name: '@srclaunch/ci-cd',
@@ -62,79 +47,6 @@ export default <LibraryConfig>{
       access: PublishAccess.Public,
       license: License.MIT,
       registry: 'https://registry.npmjs.org/',
-    },
-  },
-  repository: {
-    workflows: {
-      [RepositoryEvent.Push]: new GitHubWorkflow({
-        name: 'publish',
-        branch: 'main',
-        input: {
-          packageManager: PackageManager.PNPM,
-        },
-        tasks: {
-          vars: new Task({
-            steps: {
-              branch: new GetBranch(),
-              metadata: new GetPackageMetadata(),
-            },
-          }),
-          build: new Task({
-            id: 'build',
-            name: 'Build',
-            needs: ['vars'],
-            steps: {
-              checkout: new Checkout(),
-              setupNode: new SetupNode({ version: 16 }),
-              setupPNPM: new SetupPNPM({ installDependencies: true }),
-              buildProject: new BuildProject(),
-              buildProjectDocs: new BuildProjectDocs(),
-              uploadArtifact: new UploadArtifact({
-                name: '${{ github.event.repository.owner.login }}-${{ github.event.repository.name }}-${{ needs.vars.outputs.branch }}-${{ needs.vars.outputs.version }}-build',
-                paths: [
-                  'dist',
-                  'package.json',
-                  'README.md',
-                  'LICENSE.md',
-                  'pnpm-lock.yaml',
-                ],
-              }),
-            },
-          }),
-          test: new Task({
-            id: 'test',
-            name: 'Test',
-            needs: ['vars', 'build'],
-            steps: {
-              checkout: new Checkout(),
-              setupNode: new SetupNode({ version: 16 }),
-              setupPNPM: new SetupPNPM({ installDependencies: true }),
-              runTests: new RunTests(),
-              uploadArtifact: new UploadArtifact({
-                name: '${{ github.event.repository.owner.login }}-${{ github.event.repository.name }}-${{ needs.vars.outputs.branch }}-${{ needs.env-vars.outputs.version }}-code-coverage.json',
-                paths: ['coverage/coverage-summary.json'],
-              }),
-              uploadCoverageToCoveralls: new UploadCoverageToCoveralls({
-                token: '${{ secrets.GITHUB_TOKEN }}',
-              }),
-            },
-          }),
-          publish: new Task({
-            name: 'Publish package',
-            needs: ['vars', 'build', 'test'],
-            steps: {
-              downloadArtifact: new DownloadArtifact({
-                name: '${{ github.event.repository.owner.login }}-${{ github.event.repository.name }}-${{ needs.env-vars.outputs.branch }}-${{ needs.env-vars.outputs.version }}-build',
-              }),
-              setupNode: new SetupNode({ version: 16 }),
-              setupPNPM: new SetupPNPM(),
-              pnpmPublish: new PNPMPublish({
-                token: '${{ secrets.NPM_TOKEN }}',
-              }),
-            },
-          }),
-        },
-      }),
     },
   },
   requirements: {
