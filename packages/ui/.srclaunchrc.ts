7d4ea92e9139fb80d14687c1f2f666548ce9aa77
Steven Bennett <steven@budgetbloom.com>
2022-06-17 04:02:20 -0700

    fix(docs): add react global

--- packages/ui/.srclaunchrc.ts
+++ packages/ui/.srclaunchrc.ts
@@ -66,7 +66,7 @@ export default <ComponentLibraryConfig>{
           'styled-components',
         ],
         globals: {
-          // react: 'React',
+          react: 'React',
           'styled-components': 'styled',
         },
       },
