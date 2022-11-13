651465bad4eb20b637e97b118cebd4700869b2ea
Steven Bennett <steven@budgetbloom.com>
2022-11-13 09:29:14 -0800

    Up

--- packages/dx/.releaserc.cjs
+++ packages/dx/.releaserc.cjs
@@ -1,18 +1,21 @@
 module.exports = {
   branches: [{ name: 'main' }],
-  extends: ['semantic-release-commit-filter'],
+  extends: [
+    // 'semantic-release-commit-filter'
+  ],
   pkgRoot: 'dist',
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: true,
-        pkgRoot: process.cwd(),
-      },
-    ],
-    '@semantic-release/github',
+    '@srclaunch/ci-cd'
+    // '@semantic-release/commit-analyzer',
+    // '@semantic-release/release-notes-generator',
+    // [
+    //   '@semantic-release/npm',
+    //   {
+    //     npmPublish: true,
+    //     pkgRoot: process.cwd(),
+    //   },
+    // ],
+    // '@semantic-release/github',
   ],
   tagFormat: `v\${version}`,
 };
