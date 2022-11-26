b8c568fad4d5a4f1ba164402c8e5c6d6e0258cba
Steven Bennett <steven@budgetbloom.com>
2022-11-25 19:19:34 -0800

    try

--- packages/ci-cd/src/index.ts
+++ packages/ci-cd/src/index.ts
@@ -2,6 +2,7 @@ import SemanticRelease, { Context } from 'semantic-release';
 
 export default {
   analyzeCommits: (config: SemanticRelease.Config, context: Context) => {
+    console.log('asdf');
     return true;
     // return context.env.FORCE_PUBLISH;
   },
